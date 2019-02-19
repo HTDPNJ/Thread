6528fcf4fc934d04b55252a4d05c3e936f66fb29
HTDPNJ <TOliverQueen1@163.com>
2019-02-19 17:18:21 +0800

    Initial commit

---
+++ src/Demo01.java
@@ -0,0 +1,32 @@
+public class Demo01
+{
+    public static void main(String[] args)
+    {
+        Study s=new Study();
+        new Thread(s).start();
+        for(int i=0;i<100;i++){
+            if(50==i) {
+                s.stop();
+            }
+            System.out.println("main......"+i);
+        }
+    }
+}
+class Study implements Runnable{
+    //1、线程类中定义线程体使用的标识
+    private  boolean flag=true;
+
+    @Override
+    public void run()
+    {
+        //2、线程体中使用该标识
+        while(flag){
+            System.out.println("study thread...");
+        }
+    }
+    //3、对外提供方法改变标志
+    public void stop(){
+        this.flag=false;
+    }
+
+}
